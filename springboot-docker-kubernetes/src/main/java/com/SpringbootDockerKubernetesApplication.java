7a26cfc353c58d4e3e7e337828c4a31dd2ba2ee9
swastikpadhy <swastikpadhy@gmail.com>
2020-10-02 13:57:27 +0530

    Initial commit

---
+++ springboot-docker-kubernetes/src/main/java/com/SpringbootDockerKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerKubernetesApplication.class, args);
+	}
+
+}
